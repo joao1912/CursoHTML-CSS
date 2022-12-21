dd55476fe63485a1463afc14f924f470f549c7e0
João Pedro T <joaopedrot1912@gmail.com>
2022-12-21 10:31:16 -0300

    icone feito

---
+++ Curso HTML-CSS/exercicios Mod. (4)/ex026/mq005/script.js
@@ -0,0 +1,12 @@
+let menu = document.getElementById("menu")
+
+
+
+function clickMenu() {
+    if(menu.style.display == 'block') {
+        menu.style.display = 'none'
+    } else {
+        menu.style.display = 'block'
+    }
+
+}
