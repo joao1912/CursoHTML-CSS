6b79d84a7297e8eac5104fc198a9cfe2816ef193
João Pedro T <joaopedrot1912@gmail.com>
2022-12-22 09:57:55 -0300

    changes

--- Curso HTML-CSS/exercicios Mod. (4)/ex026/mq005/script.js
+++ Curso HTML-CSS/exercicios Mod. (4)/ex026/mq005/script.js
@@ -10,3 +10,11 @@ function clickMenu() {
     }
 
 }
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768 ) {
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+}
